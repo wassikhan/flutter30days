bca98e1636c22118f17c20a51a41f682b5d819a4
wassikhan <engineerwassikhan@gmail.com>
2021-09-10 17:27:05 +0500

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/home.dart';
 import 'package:flutter_application_2/pages/login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: GoogleFonts.lato().fontFamily),
       routes: {
         "/": (context) => Login(),
         "/home": (context) => Home(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -6,14 +7,53 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.white,
       child: Center(
-        child: Text(
-          "Welcome To Login Page", 
-          style: TextStyle(
-          color: Colors.red,
-          fontWeight: FontWeight.bold
-        ),),
-      ),
+          child: Column(
+        children: [
+          Image.asset("assets/images/login_1.png",
+              height: 150, fit: BoxFit.fill),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome To TheOnspy",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+            child: Text("Login To access your account."),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                ],
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print("Go To Home Page");
+              },
+              child: Text("Login"),
+              style: TextButton.styleFrom())
+        ],
+      )),
     );
   }
 }
